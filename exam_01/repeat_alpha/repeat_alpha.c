8cc7742c0e5a366f7587ece8177b8635bcde29a5
issaali1 <essa64069@gmail.com>
2025-08-05 01:00:07 +0200

    01/01

---
+++ exam_01/repeat_alpha/repeat_alpha.c
@@ -0,0 +1,35 @@
+#include<unistd.h>
+
+int	letter_count(char c)
+{
+	int	repeat;
+
+	if(c >= 'a' && c <= 'z')
+		repeat = c - 'a' + 1;
+	else if(c >= 'A' && c <= 'Z')
+		repeat = c - 'A' + 1;
+	else
+		repeat = 1;
+	return(repeat);	
+}
+
+int	main(int ac, char **av)
+{
+	int	repeat;
+
+	if(ac == 2)
+	{
+		while(*av[1]) //dont forget the * here   ^^
+		{                        
+			repeat = letter_count(*av[1]); //assin repeat to the function
+			while(repeat--)
+			{
+				write(1, av[1], 1);
+			}
+			av[1]++;
+		}
+	}
+	write(1, "\n", 1);
+}
+
+
