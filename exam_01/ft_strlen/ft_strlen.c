e4bcf0c5037967e3959e8e3e8f81ac07c608a448
issaali1 <essa64069@gmail.com>
2025-08-04 22:26:59 +0200

    00/01

---
+++ exam_01/ft_strlen/ft_strlen.c
@@ -0,0 +1,21 @@
+#include<unistd.h>
+#include<stdio.h>
+
+int	ft_strlen(char *str);
+int	main(void)
+{
+	char *str = "counting";
+	printf("%d", ft_strlen(str));
+	
+	return(0);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i])
+		i++;
+	return(i);
+}
