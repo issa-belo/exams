4229af8a8d2f510ca14fafa4fad701d43f759f48
issaali1 <essa64069@gmail.com>
2025-08-06 00:11:35 +0200

    01/02

---
+++ exam_01/ft_first_word/ft_first_word.c
@@ -0,0 +1,19 @@
+#include<unistd.h>
+
+int	main(int ac, char **av)
+{
+	if(ac == 1)
+	{
+		write(1, "\n", 1);
+		return(0);
+	}
+	while(*av[1] && (*av[1] == '\t' || *av[1] == ' ')) //neded to point to the first char in the strig.
+		av[1]++;
+	while(*av[1] && (*av[1] != '\0' && *av[1] != ' ' && *av[1] != '\t'))
+	{
+		write(1,av[1], 1);
+		av[1]++;
+	}
+	write(1, "\n", 1);
+	return(0);
+}
